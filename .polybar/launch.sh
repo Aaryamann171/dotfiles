3c22b665deac767f05636e4ceb83f7e25f59a865
Aaryamann <aaryamann171@gmail.com>
2020-06-17 01:05:59 +0530

    renamed polybar

--- .polybar/launch.sh
+++ .polybar/launch.sh
@@ -11,8 +11,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #Launch Polybar, using default config ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
   done
 else
-  polybar --reload example &
+  polybar --reload main &
 fi
