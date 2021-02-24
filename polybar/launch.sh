df579f2088f8b9a02ea0d04984b3401c2c68fca5
Aaryamann171 <aaryamann171@gmail.com>
2021-02-24 19:46:15 +0530

    fixes for polybar for arch setup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,18 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#Launch Polybar, using default config ~/.config/polybar/config
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload main &
-fi
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main &
+
+echo "Polybar launched..."
