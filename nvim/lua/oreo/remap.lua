f66344af2471e02fe643193ff5717425010b8817
Aaryamann171 <aaryamann171@gmail.com>
2025-03-13 16:00:46 +0530

    added nvchad config with personal tweaks

--- nvim/lua/oreo/remap.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("i", "jj", "<ESC>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', "<leader>fc", ":LspZeroFormat<CR>", { silent = true })
-vim.keymap.set('n', "<leader>n", ":NvimTreeToggle<CR>", { silent = true })
